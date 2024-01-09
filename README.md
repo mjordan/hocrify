2dc35801809b6ae7281f0f7939412b52bb9dd49c
Mark Jordan <mjordan@sfu.ca>
2024-01-08 21:55:26 -0800

    Updated README; added option to use a separate output directory.

--- README.md
+++ README.md
@@ -74,6 +74,17 @@ input/
     └── 1948-11-12-04.txt
 ```
 
+## Configuration and usage
+
+You will need to conigure the following four variables at the top of the script:
+
+* `input_dir`: the path to the page images, organized by book or newspaper issue.
+* `output_dir`: where to save the output. If left empty (`''`), the input_dir will be used.
+* `page_image_extension`: the extension of the source page image file, without the leading period, e.g. `tif`.
+* `filename_segment_separator`: the character used to separate the page number from the rest of the page image file, e.g. the `-` that separates the `02` from `1948-11-12` in `1948-11-12-02.tif`. Does not need to be the same as the separator used in the non-page number part of the filename. It also doesn't matter what comes before the page number separator.
+* `generate_ocr`: set to `True` to extract the text from the hOCR, producing the equivalent of an OCR file with no line breaks.
+* `pytesseract.pytesseract.tesseract_cmd`: the full path to the Tesseract executable, if necessary.
+
 ## Requirements
 
 - Tesseract
--- hocrify.py
+++ hocrify.py
@@ -1,27 +1,37 @@
 import os
 import re
+import shutil
 from multiprocessing import Process
 
 import pytesseract
 from bs4 import BeautifulSoup
 
 input_dir = 'input'
+output_dir = ''
 page_image_extension = 'tif'
 filename_segment_separator = '-'
 generate_ocr = True
+
+# If you don't have tesseract executable in your PATH, include the following:
+# pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_your_tesseract_executable>'
+# Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 
 # Could be books or newspaper issues.
 page_containers = os.listdir(input_dir)
 
 def generate_output(oddeven):
     for page_container in page_containers:
+        if len(output_dir) > 0:
+            if not os.path.exists(os.path.join(output_dir, page_container)):
+                os.mkdir(os.path.join(output_dir, page_container))
+
         pages = os.listdir(os.path.join(input_dir, page_container))
 
         for page in pages:
             # We distinguish between odd an even pages so each of out two processes
             # don't step on each other.
             filename_segments = os.path.splitext(page)[0].split(filename_segment_separator)
-            order_segment = filename_segments[3]
+            order_segment = filename_segments[-1]
             if int(order_segment) % 2 == 0 and oddeven == 'odd':
                 continue
             if int(order_segment) % 2 != 0 and oddeven == 'even':
@@ -29,9 +39,15 @@ def generate_output(oddeven):
 
             if page.endswith(page_image_extension):
                 page_image_filepath = os.path.join(input_dir, page_container, page)
+                if len(output_dir) > 0:
+                    shutil.copyfile(page_image_filepath, os.path.join(output_dir, page_container, page))
                 page_image_filepath_without_extension = os.path.splitext(page_image_filepath)[0]
-                page_hocr_filepath = os.path.join(page_image_filepath_without_extension + '.hocr')
-                page_ocr_filepath = os.path.join(page_image_filepath_without_extension + '.txt')
+                if len(output_dir) > 0:
+                    page_hocr_filepath = os.path.join(output_dir, page_container, os.path.splitext(page)[0] + '.hocr')
+                    page_ocr_filepath = os.path.join(output_dir, page_container, os.path.splitext(page)[0] + '.txt')
+                else:
+                    page_hocr_filepath = os.path.join(page_image_filepath_without_extension + '.hocr')
+                    page_ocr_filepath = os.path.join(page_image_filepath_without_extension + '.txt')
 
                 try:
                     # Generate hOCR and save it to a file.
@@ -64,6 +80,9 @@ def generate_output(oddeven):
                         print(f'Error generating OCR: {e}')
 
 if __name__ == "__main__":
+    if len(output_dir) > 0 and not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
     process_odd = Process(target=generate_output, args=('odd',))
     process_odd.start()
 
